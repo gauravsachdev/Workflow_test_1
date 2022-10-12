f72ece81a88b58baa3b2d2f183550845bcec37ae
Gaurav Sachdev <gaurav1565@gmail.com>
2022-10-11 23:49:03 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,4 @@
 # pandoc to convert from MarkDown to different formats
 # and saves the output into "output_temp"
 
-OUTPUT_DIR="output_temp"
-
-mkdir "$OUTPUT_DIR"
-
-dir /s /b /o:gn *.rst > "$OUTPUT_DIR"/.files.txt
-while read p; do
-  cp "$p" "$OUTPUT_DIR"
-done <"$OUTPUT_DIR"/.files.txt
+git clone https://github.com/gauravsachdev/geoedf.git
